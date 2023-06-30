6f2215047d7174099d39c5d1a479dbd4d18e48f3
g <josegonzalorives@gmail.com>
2023-06-30 17:42:23 -0300

    Base

--- README.md
+++ README.md
@@ -1,19 +1,15 @@
-# Práctica Nº 2
+# Práctica Nº 3
 
-## Formulario
+## DOM con formularios
 
-Dado el formulario a continuación:
+Dado el siguiente formulario, previamente creado en el documento HTML:
 
-![Exercise example](https://gcdnb.pbrd.co/images/lK7zhZEwD2mD.png?o=1)
+![Exercise example](https://gcdnb.pbrd.co/images/8LAubfgJXUBY.png?o=1)
 
-El usuario debe ingresar el nombre de la tarea en el cuadro de texto. Al hacer clic en el botón __Agregar__, se debe generar (programáticamente) un elemento de lista (`<li>`), e insertarse en la lista no ordenada (`<ul>`) presente en la página.
+Desarrollar un programa en Javascript que tome los datos cargados, genere un control de formulario y los inserte en el documento.
 
-Generar un documento HTML vacío (es decir, con la estructura completa, pero con el `<body>` vacío) y luego, desarrollar un programa en Javascript que utilizando los métodos y propiedades proporcionados por el DOM (`createElement`, `getElementById`, `setAttribute`, etc) genere el formulario pedido.
+Los tipos de control entre los que el usuario podrá elegir son: `text`, `password`, `number`, `color` y `email`.
 
-Algunas aclaraciones respecto del citado formulario:
+El campo _Nombre_ corresponde al atributo _name_ del control.
 
- - El campo __Edad__ debe ser de tipo numérico, es decir que sólo permita números, y éstos además deben ser mayores o iguales a dieciocho (18).
- - El campo __Ciudad__ es una lista desplegable (`<select>` o _combo box_) que debe permitir seleccionar los ítems _Libertad, Parque San Martín_ o _Merlo Centro_.
- - El botón __Enviar__ también se crea como un elemento `<input>` (como cualquier elemento de formulario) cuyo tipo es _submit_.
-
-Recordá que el cuerpo de la página HTML debe estar vacío y que todo su contenido deberá generarse usando Javascript.
+Tener en cuenta que tener una etiqueta (`<label>`) sin un ID para asociarla a un control es un error.
--- script.js
+++ script.js
@@ -1,140 +1,105 @@
 /**
- * Práctica Nº 2 - Lista de tareas
+ * Práctica Nº 3 - Controles de formulario
  *
- * @link   https://github.com/1rives/isft177-ayed3/tree/exercise2
- * @file   Script responsable de generar y agregar el titulo y formulario de la página
+ * @link   https://github.com/1rives/isft177-ayed3/tree/exercise3
+ * @file   Script responsable de crear controles de formulario
  * @author 1rives
  */
 
-// Constantes
-const selectOptions = ['Seleccione una opción', 'Libertad', 'Parque San Martín', 'Merlo Centro'];
-const nameID = 'nameInput';
-const ageID = 'ageInput';
-const cityID = 'cityInput';
-
-document.addEventListener('submit', (event) =>  {
-    event.preventDefault();
-    
-    let successMessage = document.createElement('p');
-    successMessage.append('👌');
-
-    document.body.append(successMessage);    
-});
-
 document.addEventListener('DOMContentLoaded', () => {
 
-    // TODO: Mostrar todo el contenido antes del script
+    // Constantes
+    const formControl = document.getElementById('formControl');
 
-    // Titulo
-    let title = document.createElement('h1');
-    title.append('Formulario de alta de personas');
+    const controlType = document.getElementById('controlType');
+    const controlName = document.getElementById('controlName');
+    const controlId = document.getElementById('controlId');
+    const controlLabel = document.getElementById('controlLabel');
 
-    // Formulario
-    let form = document.createElement('form');
+    const newControl = document.getElementById('newControl');
+    const errorMessage = document.getElementById('errorMessage');
 
-    // Labels
-    let nameLabel = document.createElement('label');
-    let ageLabel = document.createElement('label');
-    let cityLabel = document.createElement('label');
-    addLabelAttributes(nameLabel, ageLabel, cityLabel);
-    
-    // Inputs
-    let nameInput = document.createElement('input');
-    let ageInput = document.createElement('input');
-    let cityInput = document.createElement('select');
-    addInputAttributes(nameInput, ageInput, cityInput);
-    
-    // Botón "submit"
-    let submitButton = document.createElement('input');
-    submitButton.append('Ingresar');
-    submitButton.setAttribute('id', 'submit');
-    submitButton.setAttribute('type', 'submit');
-    
-    // Agrego la base
-    document.body.append(title, form);
 
-    // Incluyo todos los elementos dentro del formulario
-    form.append(nameLabel, nameInput, ageLabel, ageInput, cityLabel, cityInput, submitButton);
+    formControl.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const newId = controlId.value;
+        const newLabel = controlLabel.value;
+
+        if(!newId && newLabel){
+            // TODO: Function that shows error message, when there's 
+            // an existing message, replace with the new one.
+            errorMessage.append('Ingrese una ID');
+            return;
+        }
+
+        createControlElements(newId, newLabel, newControl);
+        
+    });
+
+    newControl.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        alert('...');
+    });
 
 });
 
 /**
- * Agrega opciones al select de "city"
- *
- * Crea opciones con sus respectivos atributos y las adjunta una por una 
- * dentro del elemento "form".
+ * Crea nuevos elementos para nuevo formulario de control
  * 
- * La primera opción siempre es el placeholder.
- *
- * @param {array} options               Lista de ciudades
- * @param {HTMLSelectElement} input     Elemento HTML de select "cityInput"
+ * @param {string} id                           Valor de ID ingresado
+ * @param {string} label                        Valor de etiqueta ingresado
+ * @param {string} controlDiv        Div donde se agregan los elementos 
  */
-function addCityOptions(options, input) {
-    for (let index = 0; index < options.length; index++) {
-        // Creo nueva option
-        let option = document.createElement('option');
-        let optionText = document.createTextNode(options[index]);
-        
-        // Agrego el texto de la option
-        option.appendChild(optionText);
-        
-        // Defino atributos de la option
-        if(!index) {
-            option.setAttribute('value', '');
-            option.setAttribute('selected', 'selected');
-            option.setAttribute('hidden', true);
-            option.setAttribute('disabled', true);
-        }
-        else {
-            option.setAttribute('value', `city${index+1}`);
-        }
-        
-        input.append(option);
+function createControlElements(id, label, controlDiv) {
+    let newLabel = document.createElement('label');
+    let newInput = document.createElement('input');
+    let newSubmit = document.createElement('button');
+
+    // TODO: FIX ALL
+
+    // Input
+    newInput.setAttribute('type', controlType.value);
+    
+    newInput.setAttribute('name', controlName.value);
+    newInput.setAttribute('placeholder', 'Valor ' + controlType.value);
+
+    // Boton
+    newSubmit.setAttribute('id', 'newSubmit');
+    newSubmit.append('Validar');
+
+    // En el caso de que se haya ingresado un Label 
+    // tambien crear su elemento
+    if(label) {
+        // Etiqueta
+        newLabel.setAttribute('for', controlId.value);
+        newLabel.append('Ingrese un nuevo valor');
     }
+
+    // En el caso de que se haya ingresado un ID válido 
+    // asignarlo como ID del nuevo Input
+    if(id) {
+        newInput.setAttribute('id', controlId.value);
+    }
+
+    // Agrego los nuevos elementos con controles
+    addNewControlElements({newLabel, newInput, newSubmit}, controlDiv);
 }
 
-/**
- * Asigna atributos a todos los inputs disponibles
- *
- * @param {HTMLSelectElement} nameInput    Elemento HTML de nombre/apellido
- * @param {HTMLSelectElement} ageInput     Elemento HTML de edad
- * @param {HTMLSelectElement} cityInput    Elemento HTML del select de ciudades
- */
-function addInputAttributes(nameInput, ageInput, cityInput) {
-
-    nameInput.setAttribute('id', nameID);
-    nameInput.setAttribute('type', 'text');
-    nameInput.setAttribute('placeholder', 'Ingrese nombre y apellido');
-    nameInput.setAttribute('minlength', '3');
-    nameInput.setAttribute('maxlength', '50');
-    nameInput.setAttribute('required', true);
-
-    ageInput.setAttribute('id', ageID);
-    ageInput.setAttribute('type', 'number');
-    ageInput.setAttribute('placeholder', 'Ej.: 55');
-    ageInput.setAttribute('min', '18');
-    ageInput.setAttribute('max', '150');
-    ageInput.setAttribute('required', true);
-
-    cityInput.setAttribute('id', cityID);
-    cityInput.setAttribute('required', true);
-    addCityOptions(selectOptions, cityInput);
-}
 
 /**
- * Asigna atributos a todos los labels disponibles
- *
- * @param {HTMLSelectElement} nameLabel    Elemento HTML del label para nombre/apellido
- * @param {HTMLSelectElement} ageLabel     Elemento HTML del label para edad
- * @param {HTMLSelectElement} cityLabel    Elemento HTML del label para el select de ciudades
+ * Recibo un array con los elementos a agregar
+ * 
+ * @param {array} elements                     Lista de elementos a agregar
+ * @param {HTMLSelectElement} controlDiv       Div donde se agregan los elementos 
  */
-function addLabelAttributes(nameLabel, ageLabel, cityLabel) {
-    nameLabel.append('Nombre y Apellido');
-    nameLabel.setAttribute('for', nameID);
+function addNewControlElements(elements, controlDiv) {
 
-    ageLabel.append('Edad');
-    ageLabel.setAttribute('for', ageID);
-
-    cityLabel.append('Ciudad');
-    cityLabel.setAttribute('for', cityID);
+    for (let i = 0; i < elements.length; index++) {
+        // Si esta vacio, no agregar
+        if(elements[index] !== ''){
+            controlDiv.appendChild(elements[index]);
+        }
+    }
 }
